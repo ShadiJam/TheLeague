1dcdf88d9316e0981a76f5f825c8fdc8951ad1aa
Shadi Jam <shadijam00@gmail.com>
2016-10-14 14:08:16 -0500

    teams created

--- TheLeague/Program.cs
+++ TheLeague/Program.cs
@@ -13,32 +13,45 @@ namespace TheLeague
         
         static void Main(string[] args)
         {
-            var leagueSport = new Sport("Basketball");
+            var sportLeague = new Sport("Basketball");
             var leagueName = new League("National Basketball Association");
-            const string DATA1 = "*Rockets*, _Mike D'Antoni_, _Jeff Bzdelik_, Ryan Anderson, Trevor Ariza, Patrick Beverley, Corey Brewer, James Hardon";
-            const string DATA2 = "*Bulls*, _Fred Hoiberg_, _Randy Brown_, Jimmy Butler, Tah Gibson, Rajon Rondo, Tony Snell, Robin Lopez";
-            const string DATA3 = "*Knicks*, _Jeff Hornacek_, _Kurt Rambis_, Derrick Williams, Justin Holiday, Thomas Lance, Brandon Jennings, Carmelo Anthony";
+            const string DATA1 = "*Rockets*, 'Houston', _Mike D'Antoni_, _Jeff Bzdelik_, -Ryan Anderson-, |1124|, -Trevor Ariza-, |1025|, -Patrick Beverley-, |700|, -Corey Brewer-, |590|, -James Hardon-, |2376|";
+            const string DATA2 = "*Bulls*, 'Chicago', _Fred Hoiberg_, _Randy Brown_, -Jimmy Butler-, |1399|, -Taj Gibson-, |1935|, -Rajon Rondo-, |2536|, -Tony Snell-, |338|, -Robin Lopez-, |842|";
+            const string DATA3 = "*Knicks*, 'New York', _Jeff Hornacek_, _Kurt Rambis_, -Derrick Williams-, |746|, -Justin Holiday-, |239|, -Thomas Lance-, |484|, -Brandon Jennings-, |175|, -Carmelo Anthony-, |1573|";
             List<Team> teams = new List<Team>();
             List<Coach> coaches = new List<Coach>();
             List<Player> players = new List<Player>();
-            Team lastteam = null;
-            Coach lastcoach = null;
-            Player lastplayer = null;
+            Team lastTeam = null;
+            Coach lastCoach = null;
+            Player lastPlayer = null;
             Action<string> process = str =>
             {
                 if (str.IndexOf("*") == 0)
                 {
-                    lastteam = new Team(str.Substring(1, str.Length - 1));
-                    teams.Add(lastteam);
-                } else if (str.IndexOf("_") == 0)
-                {
-                    lastcoach = new Coach(str.Substring(1, str.Length - 1));
-                    coaches.Add(lastcoach);
-                } else
-                {
-                    var p = new Player[] { new Player(str) };
-                    players.Add(lastplayer);
+                    lastTeam = new Team(str.Substring(1, str.Length - 1));
+                    teams.Add(lastTeam);
                 }
+                else if (str.IndexOf("'") == 0)
+                {
+                    lastTeam.homeTown = (str.Substring(1, str.Length -1));
+                    teams.Add(lastTeam);
+                }
+                else if (str.IndexOf("_") == 0)
+                {
+                    lastCoach = new Coach(str.Substring(1, str.Length - 1));
+                    coaches.Add(lastCoach);
+                }
+                else if (str.IndexOf("-") == 0)
+                {
+                    lastPlayer = new Player(str.Substring(1, str.Length - 1));
+                    players.Add(lastPlayer);
+                }
+                else if (str.IndexOf("|") == 0)
+                {
+                    lastPlayer.points = int.Parse(str.Substring(1, str.Length - 1));
+                    players.Add(lastPlayer);
+                }
+                
             };
             foreach(string s in DATA1.Split(new char[] { ',' }))
             {
@@ -52,12 +65,14 @@ namespace TheLeague
             {
                 process(s.Trim());
             }
+        //    Directory.CreateDirectory("html");
+        //    File.WriteAllText(@"html/index.html", ToString)
             
             Console.WriteLine(teams);
             Console.ReadLine();
         }
         class Sport {
-            public string name { get; private set; }
+            public string name; 
             public Sport(string name)
             {
                 this.name = name;
@@ -75,7 +90,7 @@ namespace TheLeague
             }
         }
         class League {
-            public string name { get; private set; }
+            public string name; 
             public League(string name)
             {
                 this.name = name;
@@ -83,26 +98,48 @@ namespace TheLeague
             IEnumerable<Team> teams = new List<Team>();
             public override string ToString()
             {
-                return $"{name}";
+                string t = String.Join(", ", teams);
+                return String.Format(@"
+                <div>
+                    <h1>{0}</h1>
+                        <p> Teams: {1} </p>
+                </div>
+                ", name, t);
             }
         }
         class Team {
-            public string name { get; private set; }
-            public string hometown;
-            public Team(string name = "", string hometown = "")
+            public string name { get; set; }
+            public string homeTown;
+            public Team(string name)
             {
                 this.name = name;
-                this.hometown = hometown;
             }
             IEnumerable<Coach> coaches = new List<Coach>();
             IEnumerable<Player> players = new List<Player>();
             public override string ToString()
             {
-                return $"{name}";
+                string t = String.Join(", ", name);
+                string c = String.Join(", ", coaches);
+                string p = String.Join(", ", players);
+                return String.Format(
+                @"<!DOCTYPE html>
+                <html>
+                <head>
+                    <title></title>
+                    <link href=""html/index.html"" type=""text/css"" rel=""stylesheet"">
+                </head>
+                <body> 
+                    <h1> {0} </h1>
+                        <p> Team Hometown: {2} </p>
+                        <p> Coaches: {3} </p>
+                        <p> Players: {4} </p>
+                </body>
+                </html>
+                ", name, homeTown, c, p);
             }
         }
         class Coach {
-            public string name { get; private set; }
+            public string name { get; set; }
             public Coach(string name)
             {
                 this.name = name;
@@ -121,7 +158,7 @@ namespace TheLeague
             }
             public override string ToString()
             {
-                return $"{name}";
+                string t = String.Join(", ", team);
             }
         }
 
