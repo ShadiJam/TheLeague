ac293db099911825ab01883e0f1bf9da920e8bdf
Shadi Jam <shadijam00@gmail.com>
2016-10-18 21:50:18 -0500

    adding player of the year

--- TheLeague/Program.cs
+++ TheLeague/Program.cs
@@ -13,7 +13,7 @@ namespace TheLeague
 
         static void Main(string[] args)
         {
-            var sportLeague = new Sport("Basketball");
+            
             var leagueName = new League("National Basketball Association");
             const string DATA1 = "*Rockets*, 'Houston', _Mike D'Antoni_, _Jeff Bzdelik_, -Ryan Anderson-, |1124|, -Trevor Ariza-, |1025|, -Patrick Beverley-, |700|, -Corey Brewer-, |590|, -James Hardon-, |2376|";
             const string DATA2 = "*Bulls*, 'Chicago', _Fred Hoiberg_, _Randy Brown_, -Jimmy Butler-, |1399|, -Taj Gibson-, |1935|, -Rajon Rondo-, |2536|, -Tony Snell-, |338|, -Robin Lopez-, |842|";
@@ -43,8 +43,8 @@ namespace TheLeague
                     lastTeam.coaches = lastTeam.coaches.Concat(new Coach[] { lastCoach });
                 }
                 else if (str.IndexOf("-") == 0)
-                {
-                    lastPlayer = new Player(str.Substring(1, str.Length - 1));
+                {       // not sure how to fix this! How else do I specify that this is the string name?
+                    lastPlayer.name = new Player(str.Substring(1, str.Length - 1)); 
                     players.Add(lastPlayer);
                     lastTeam.players = lastTeam.players.Concat(new Player[] { lastPlayer });
                 }
@@ -52,7 +52,6 @@ namespace TheLeague
                 {
                     lastPlayer.points = int.Parse(str.Substring(1, str.Length - 1));
                     players.Add(lastPlayer);
-                  //  lastPlayer.points = lastPlayer.points.Concat(new Player[] { points });
                 }
             };
             foreach (string s in DATA1.Split(new char[] { ',' }))
@@ -70,18 +69,56 @@ namespace TheLeague
             Directory.CreateDirectory("html");
 
             File.WriteAllText(@"html/index.html", teams.ElementAt(0).ToString());
-            var l = new League();
-            for(var m = 0; m<2; m++)
-            {
-                var t = new Team();
-                l.teams = l.teams.Concat(new List<Team> { t });
+            //creates list of players in league
+            var sportLeague = new Sport("Basketball");
+            for(var k=0; k<1; k++) {
+                var l = new League("");
+                sportLeague.leagues = sportLeague.leagues.Concat(new List<League> { l });
+                    //for each of the three teams
+                    for (var n = 0; n < 3; n++)
+                    {
+                        var t = new Team("");
+                        l.teams = l.teams.Concat(new List<Team> { t });
 
-                for(var n = 0; n<3; n++)
-                {
-                    var p = new Player();
-                    t.players = t.players.Concat(new List<Player> { p });
+                        //for each of the five players in each team
+                        for (var m = 0; m < 5; m++) {
+                        var p = new Player("", 0);
+                        t.players = t.players.Concat(new List<Player> { p });
+                    }
                 }
             }
+            //concats list of players points from the league to the players in each team 
+            Func<Team, IEnumerable<Player>> getPlayersFromTeam = (t) => t.players;
+            Func<League, IEnumerable<Player>> getPlayersFromLeague = (l) =>
+            {
+                return l.teams.Aggregate(new List<Player>(),
+                    (acc, t) => acc.Concat(getPlayersFromTeam(t)).ToList());
+            };
+            Func<Sport, IEnumerable<Player>> getAllPlayers = (s) =>
+            {
+                return s.leagues.Aggregate(new List<Player>(),
+                    (acc, l) => acc.Concat(getPlayersFromLeague(l)).ToList());
+            };
+            IEnumerable<Player> allPlayers = getAllPlayers(sportLeague);
+            foreach(var p in players)
+            {
+                Console.WriteLine($"{p.name}{p.points}");
+            }
+            Func<Sport, Player> playerOfTheYear = (sport) =>
+            {
+                Player maxPoints = new Player("", 0); 
+                foreach (Player p in getAllPlayers(sport))
+                {
+                    if (p.points > maxPoints.points)
+                    {
+                        maxPoints = p;
+                    }
+                }
+                return maxPoints;
+            };
+            Console.WriteLine(playerOfTheYear(sportLeague).points);
+            // need to add in a Func to create averages of each team and then getCoachOfTheYear
+            // need to figure out how to print this to the damn console!
         }
     }
 
@@ -100,90 +137,49 @@ namespace TheLeague
             Football,
         }
         public IEnumerable<League> leagues = new List<League>();
-        public string List<Team> getAllTeams()
-        {
-            
-        }
-
-
-        public static string getplayerOfTheYear()
-        {
-            List<Player> result = new List<Player>();
-            Player getPlayerOfTheYear = players.Max();
-            foreach (string player in players)
-            {
-                result.ToString();
-            }
-            return getPlayerOfTheYear.ToString();
-        }
-            
-            public int Coach getCoachOfTheYear()
-            {
-            List<Coach> result = new List<Coach>();
-            Coach getCoachOfTheYear = coaches.Add();
-            foreach(int coach in coaches)
-            {
-                result.Add();
-            }
-            return coaches.Add.Max();
-            }
-
-        
        
-            public static int Max(IEnumerable<int> players) //don't need this if using aggregates.
-            {
-            List<Player> result = new List<Player>();
-            Player max = result.Max();
-            foreach (int points in players)
+        public static int Max(IEnumerable<int> players) //don't need this if using aggregates.
+        {
+        List<Player> result = new List<Player>();
+        Player max = result.Max();
+        foreach (int points in players)
             {
                 result.Max();
             }
-                return players.Max();
-            }
-    
-            public override string ToString()
+            return players.Max();
+        }
+        public override string ToString()
             {
                 return $"{name}";
             }
             
         }
-        class League
+    class League
         {
-            public string name;
-            public League(string name)
+        public string name;
+        public League(string name)
             {
                 this.name = name;
             }
-            public IEnumerable<Team> teams = new List<Team>();
+        public IEnumerable<Team> teams = new List<Team>();
             
-            public override string ToString()
+        public override string ToString()
             {
                 return $"{name}";
             }
         }
-        class Team
+    class Team
         {
-            
-            public string name;
-            public string homeTown;
-            public Team(string name)
+        public string name;
+        public string homeTown;
+        public Team(string name)
             {
                 this.name = name;
             }
-            public IEnumerable<Coach> coaches = new List<Coach>();
-            public IEnumerable<Player> players = new List<Player>();
-           // public static List<U> teams<T, U>(IEnumerable<T> list, Func<T, U> fn)
-           // {
-           //     List<U> result = new List<U>();
-           //     foreach (var points in list)
-           //     {
-            //        result.Add(fn(points));
-            //    }
-            //        return teams.Add();
-           // } translate this into select
-           
-
-            public override string ToString()
+        public IEnumerable<Coach> coaches = new List<Coach>();
+        public IEnumerable<Player> players = new List<Player>();
+        
+        public override string ToString()
             {
                 string c = String.Join(", ", coaches);
                 string p = String.Join(", ", players);
@@ -196,28 +192,29 @@ namespace TheLeague
             ", homeTown, name, c, p);
             }
         }
-        class Coach
+    class Coach
         {
-            public string name;
-            public Coach(string name)
+         public string name;
+         public Coach(string name)
             {
                 this.name = name;
             }
-            public override string ToString()
+         public override string ToString()
             {
                 return $"{name}";
             }
         }
-        class Player
+    class Player
         {
-            public string name;
-            public int points; 
-            public Player(string name)
+        public string name;
+        public int points; 
+        public Player(string name, int points)
             {
                 this.name = name;
+                this.points = points;
             }
           
-            public override string ToString()
+        public override string ToString()
             { 
             string n = String.Join(", ", name);
             string p = String.Join(", ", points);
